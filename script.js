7d297d92013c02c9509e8a7ae0cd560214268a53
ealbanca <ealbanca@byui.edu>
2025-06-03 20:45:46 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Create a form container
+var formContainer = document.createElement('div');
+formContainer.className = 'form-container';
+//Create heading: add note
+var heading = document.createElement('h1');
+heading.textContent = 'Note Taking App';
+formContainer.appendChild(heading);
+// Add form container to document body
+document.body.appendChild(formContainer);
---
+++ script.ts
@@ -0,0 +1,13 @@
+// Create a form container
+const formContainer = document.createElement('div');
+formContainer.className = 'form-container';
+
+
+//Create heading: add note
+const heading = document.createElement('h1');
+heading.textContent = 'Note Taking App';
+formContainer.appendChild(heading);
+
+
+// Add form container to document body
+document.body.appendChild(formContainer);
