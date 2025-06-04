0edaba12e727ad7f70a2262c17680a11c0595297
ealbanca <ealbanca@byui.edu>
2025-06-03 20:53:26 -0600

    Created form container added titles and labels

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 // Create a form container
 var formContainer = document.createElement('div');
 formContainer.className = 'form-container';
-//Create heading: add note
+//Create title of Note Taking App
 var heading = document.createElement('h1');
 heading.textContent = 'Note Taking App';
 formContainer.appendChild(heading);
+//Add a ne note title
+var noteTitle = document.createElement('h3');
+noteTitle.textContent = 'Add a New Note ';
+formContainer.appendChild(noteTitle);
+// Create a form element
+var form = document.createElement('form');
+form.className = 'note-form';
+// Create a text input for the note title
+var titleInput = document.createElement('input');
+titleInput.type = 'text';
+titleInput.id = 'title';
+titleInput.placeholder = 'Enter a Note Title';
+form.appendChild(titleInput);
+formContainer.appendChild(form);
 // Add form container to document body
 document.body.appendChild(formContainer);
--- script.ts
+++ script.ts
@@ -3,11 +3,28 @@ const formContainer = document.createElement('div');
 formContainer.className = 'form-container';
 
 
-//Create heading: add note
+//Create title of Note Taking App
 const heading = document.createElement('h1');
 heading.textContent = 'Note Taking App';
 formContainer.appendChild(heading);
 
+//Add a ne note title
+const noteTitle = document.createElement('h3');
+noteTitle.textContent = 'Add a New Note ';
+formContainer.appendChild(noteTitle);
+// Create a form element
+const form = document.createElement('form');
+form.className = 'note-form';
+
+// Create a text input for the note title
+const titleInput = document.createElement('input');
+titleInput.type = 'text';
+titleInput.id = 'title';
+titleInput.placeholder = 'Enter a Note Title';
+form.appendChild(titleInput);
+
+//Append form to form container
+formContainer.appendChild(form);
 
 // Add form container to document body
 document.body.appendChild(formContainer);
